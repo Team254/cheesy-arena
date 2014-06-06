1718bdc36724873ac0459a94ae9a69e909f31363
Nick Eyre <nick@nickeyre.com>
2014-06-05 22:59:45 -0700

    Refactor Rankings Page to Eliminate Angular

--- static/js/rankings.js
+++ static/js/rankings.js
@@ -1,70 +1,39 @@
 // Copyright 2014 Team 254. All Rights Reserved.
 // Author: nick@team254.com (Nick Eyre) 
 
-'use strict';
+initialize();
 
-var app = angular
-.module('cheesyArenaRankings', []);
+var template;
+function initialize(){
+  getData(populateView);
+  template = Handlebars.compile($('#row-template').html());
+}
 
-app.controller('RankingsController', ['$scope', 'Rankings', '$interval', function($scope, Rankings, $interval){
-  
-  $scope.date = new Date();
+var rankings;
+function getData(callback){
+  $.getJSON('/reports/json/rankings', function(data){
+    data = {teams: data};
+    rankings = data;
+    if(typeof(callback) == "function")
+      callback();
+    var date = new Date();
+    console.log("New Data Acquired\n"+date);
+  });
+}
 
-  // Load Rankings
-  init();
-  function init(){
-    Rankings.get().then(function(data){
-      $scope.rankingsOld = data;
-      $scope.rankingsNew = data;
-      setTimeout(equalize, 0);
-      setTimeout(detectListSize, 0);
-    });
-  }
+function populateView(){
+  $('#container table').html(template(rankings));
+  equalize(true);
+  scroll();
+}
 
-  // Detect if List is Long Enough to Require Scrolling
-  function detectListSize(){
-    $('.new').hide();
-    if($('#container table').height() > $('#container').height()){
-      $('.new').show();
-      setTimeout(scrollToBottom, 0);
-    }
-  }
-
-  // Scroll to Bottom of List
-  function scrollToBottom(){
-    var offset = $('#container').offset().top;
-    $('#container').animate({scrollTop: $('.old').first().offset().top - offset}, 0);
-    // $scope.interval = $interval(loadNewData, 10);
-    var time = 1000 * $scope.rankingsOld.length;
-    $('#container').animate({scrollTop: $('.new').first().offset().top - offset}, time, 'linear', scrollComplete);
-  }
-
-  // Go Back to Top
-  function scrollComplete(){
-    $scope.rankingsOld = $scope.rankingsNew;
-    setTimeout(scrollToBottom, 0);
-  }
-
-  // Load New Data When 2 Elements Away from End of List
-  function loadNewData(){
-    var position = $(window).scrollTop()+$(window).height();
-    var offset = $('.old:last').prev().prev().offset().top;
-    if(position >= offset){
-      Rankings.get().then(function(data){
-        $scope.rankingsOld = $scope.rankingsNew;
-        $scope.rankingsNew = data;
-        $interval.cancel($scope.interval);
-        setTimeout(equalize, 0);
-      });
-    }
-  }
-
-  // Balance Column Widths
-  function equalize(){
+// Balance Column Widths
+var widths = [];
+function equalize(all){
+  if(all){
     var width = $('#container table').width();
     var count = $('#container tr').first().children('td').length;
     var offset = ($(window).width() - width) / (count + 1);
-    var widths = [];
     $('#container tr').first().children('td').each(function(){
       var width = $(this).width()+offset;
       $(this).width(width);
@@ -73,26 +42,35 @@ app.controller('RankingsController', ['$scope', 'Rankings', '$interval', functio
     $('#header').children('td').each(function(index){
       $(this).width(widths[index]);
     });
-    $('#container tr.new').children('td').each(function(index){
-      $(this).width(widths[index]);
-    });
   }
+  $('#container table#new tr').first().children('td').each(function(index){
+    $(this).width(widths[index]);
+  });
+}
 
-}]);
+var SCROLL_SPEED = 1000;
+function scroll(){
+  $('#container').scrollTop(0);
 
-app.factory('Rankings', ['$http', '$log', '$q', function($http, $log, $q){
-  return {
-    get: function(){
-      var deferred = $q.defer();
-      $http.get('/reports/json/rankings').
-        success(function(data, status, headers, config) {
-          // data[0]['TeamId'] = jQuery.now();
-          deferred.resolve(data);
-        }).
-        error(function(data, status, headers, config) {
-          $log.error(data);
-        });
-        return deferred.promise;
-    }
-  };
-}]);
+  var offset = $('table#new').offset().top - $('#container').offset().top;
+  var scrollTime = SCROLL_SPEED * $('table#old tr').length;
+  $('#container').animate({scrollTop: offset}, scrollTime, 'linear', reset);
+
+  $('#container table#new').html(template(rankings));
+  equalize();
+
+  interval = setInterval(pollForUpdate, POLL_INTERVAL);
+}
+
+function reset(){
+  $('#container table#old').html($('#container table#new').html());
+  scroll();
+}
+
+var POLL_INTERVAL = 1000;
+function pollForUpdate(){
+  if($('#container').offset().top * $('#container').height() > $('#container table#old tr').last().prev().offset().top){
+    getData();
+    clearInterval(interval);
+  }
+}
