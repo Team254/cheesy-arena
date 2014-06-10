87d9c71184f94fcc58ea63debf6b800bb2602d2c
Nick Eyre <nick@nickeyre.com>
2014-06-09 21:25:15 -0700

    Add Control Panel

--- static/js/audience.js
+++ static/js/audience.js
@@ -5,38 +5,39 @@ var handle;
 
 function logoScreen(){
   // Initialize
-  $('#logoScreen .blinds.center:not(.blank)').css({rotateY: '-180deg'})
+  $('#container').html($('.template#logoScreen').html());
+  $('.blinds.center:not(.blank)').css({rotateY: '-180deg'})
 
   // In Animation
-  closeBlinds('logoScreen', function(){
+  closeBlinds(function(){
     setTimeout(function(){
-      $('#logoScreen .blinds.center.blank').transition({rotateY: '180deg'});
-      $('#logoScreen .blinds.center:not(.blank)').transition({rotateY: '0deg'});
+      $('.blinds.center.blank').transition({rotateY: '180deg'});
+      $('.blinds.center:not(.blank)').transition({rotateY: '0deg'});
     }, 400);
   });
 
   // Close Function
   return function(callback){
-    $('#logoScreen .blinds.center.blank').transition({rotateY: '360deg'});
-    $('#logoScreen .blinds.center:not(.blank)').transition({rotateY: '180deg'}, function(){
-      openBlinds('logoScreen', callback);
+    $('.blinds.center.blank').transition({rotateY: '360deg'});
+    $('.blinds.center:not(.blank)').transition({rotateY: '180deg'}, function(){
+      openBlinds(callback);
     });
   }
 }
 
-function closeBlinds(id, callback){
-  $('#'+id+' .blinds.right').transition({right: 0});
-  $('#'+id+' .blinds.left').transition({left: 0}, function(){
+function closeBlinds(callback){
+  $('.blinds.right').transition({right: 0});
+  $('.blinds.left').transition({left: 0}, function(){
     $(this).addClass('full');
     callback();
   });
 }
 
-function openBlinds(id, callback){
-  $('#'+id+' .blinds.right').show();
-  $('#'+id+' .blinds.left').removeClass('full');
-  $('#'+id+' .blinds.right').show().transition({right: '-50%'});
-  $('#'+id+' .blinds.left').transition({left: '-50%'}, function(){
+function openBlinds(callback){
+  $('.blinds.right').show();
+  $('.blinds.left').removeClass('full');
+  $('.blinds.right').show().transition({right: '-50%'});
+  $('.blinds.left').transition({left: '-50%'}, function(){
     callback();
   });
 }
