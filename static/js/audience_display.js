60fe946247196cc53393e50bbfd79be753e281c6
Patrick Fairbank <pat@patfairbank.com>
2015-08-22 16:17:09 -0700

    Apply Chezy-Champs-specific changes from 2015.

--- static/js/audience_display.js
+++ static/js/audience_display.js
@@ -138,8 +138,8 @@ var transitionBlankToIntro = function(callback) {
 };
 
 var transitionIntroToInMatch = function(callback) {
-  $("#logo").transition({queue: false, top: "35px"}, 500, "ease");
-  $(".score").transition({queue: false, width: "260px"}, 500, "ease", function() {
+  $("#logo").transition({queue: false, top: "25px"}, 500, "ease");
+  $(".score").transition({queue: false, width: "230px"}, 500, "ease", function() {
     $(".score-number").transition({queue: false, opacity: 1}, 750, "ease");
     $(".score-fields").transition({queue: false, opacity: 1}, 750, "ease");
     $("#matchTime").transition({queue: false, opacity: 1}, 750, "ease", callback);
@@ -159,8 +159,8 @@ var transitionIntroToBlank = function(callback) {
 var transitionBlankToInMatch = function(callback) {
   $("#centering").transition({queue: false, bottom: "0px"}, 500, "ease", function() {
     $(".teams").transition({queue: false, width: "75px"}, 100, "linear", function() {
-      $("#logo").transition({queue: false, top: "35px"}, 500, "ease");
-      $(".score").transition({queue: false, width: "260px"}, 500, "ease", function() {
+      $("#logo").transition({queue: false, top: "25px"}, 500, "ease");
+      $(".score").transition({queue: false, width: "230px"}, 500, "ease", function() {
         $("#eventMatchInfo").show();
         $(".score-number").transition({queue: false, opacity: 1}, 750, "ease");
         $(".score-fields").transition({queue: false, opacity: 1}, 750, "ease");
@@ -176,7 +176,7 @@ var transitionInMatchToIntro = function(callback) {
   $(".score-number").transition({queue: false, opacity: 0}, 300, "linear");
   $(".score-fields").transition({queue: false, opacity: 0}, 300, "linear");
   $("#matchTime").transition({queue: false, opacity: 0}, 300, "linear", function() {
-    $("#logo").transition({queue: false, top: "50px"}, 500, "ease");
+    $("#logo").transition({queue: false, top: "45px"}, 500, "ease");
     $(".score").transition({queue: false, width: "120px"}, 500, "ease");
     $(".teams").transition({queue: false, width: "75px"}, 500, "ease", callback);
   });
@@ -188,7 +188,7 @@ var transitionInMatchToBlank = function(callback) {
   $(".score-fields").transition({queue: false, opacity: 0}, 300, "linear");
   $(".score-number").transition({queue: false, opacity: 0}, 300, "linear", function() {
     $("#eventMatchInfo").hide();
-    $("#logo").transition({queue: false, top: "50px"}, 500, "ease");
+    $("#logo").transition({queue: false, top: "45px"}, 500, "ease");
     $(".score").transition({queue: false, width: "0px"}, 500, "ease");
     $(".teams").transition({queue: false, width: "40px"}, 500, "ease", function() {
       $("#centering").transition({queue: false, bottom: "-340px"}, 1000, "ease", callback);
