8faf6f74bfdc7117ae05c093b985a26159bbb7e9
Jeff <jwspragg@mtu.edu>
2021-10-13 02:28:47 -0700

    First draft of new ranking placement on final score screen

--- static/js/audience_display.js
+++ static/js/audience_display.js
@@ -96,9 +96,12 @@ var handleRealtimeScore = function(data) {
 // Handles a websocket message to populate the final score data.
 var handleScorePosted = function(data) {
   $("#" + redSide + "FinalScore").text(data.RedScoreSummary.Score);
-  $("#" + redSide + "FinalTeam1").html(data.Match.Red1 + "" + getRankingText(data.Match.Red1, data.Rankings));
-  $("#" + redSide + "FinalTeam2").html(data.Match.Red2 + "" + getRankingText(data.Match.Red2, data.Rankings));
-  $("#" + redSide + "FinalTeam3").html(data.Match.Red3 + "" + getRankingText(data.Match.Red3, data.Rankings));
+//  $("#" + redSide + "FinalTeam1").html(data.Match.Red1 + "" + getRankingText(data.Match.Red1, data.Rankings));
+//  $("#" + redSide + "FinalTeam2").html(data.Match.Red2 + "" + getRankingText(data.Match.Red2, data.Rankings));
+//  $("#" + redSide + "FinalTeam3").html(data.Match.Red3 + "" + getRankingText(data.Match.Red3, data.Rankings));
+  $("#" + redSide + "FinalTeam1").html(getNewRankingText(data.Match.Red1, data.Rankings) + "" + data.Match.Red1);
+  $("#" + redSide + "FinalTeam2").html(getNewRankingText(data.Match.Red2, data.Rankings) + "" + data.Match.Red2);
+  $("#" + redSide + "FinalTeam3").html(getNewRankingText(data.Match.Red3, data.Rankings) + "" + data.Match.Red3);
   $("#" + redSide + "FinalTeam1Avatar").attr("src", getAvatarUrl(data.Match.Red1));
   $("#" + redSide + "FinalTeam2Avatar").attr("src", getAvatarUrl(data.Match.Red2));
   $("#" + redSide + "FinalTeam3Avatar").attr("src", getAvatarUrl(data.Match.Red3));
@@ -112,9 +115,12 @@ var handleScorePosted = function(data) {
   $("#" + redSide + "FinalEndgameRankingPoint").html(data.RedScoreSummary.EndgameRankingPoint ? "&#x2714;" : "&#x2718;");
   $("#" + redSide + "FinalEndgameRankingPoint").attr("data-checked", data.RedScoreSummary.EndgameRankingPoint);
   $("#" + blueSide + "FinalScore").text(data.BlueScoreSummary.Score);
-  $("#" + blueSide + "FinalTeam1").html(data.Match.Blue1 + "" + getRankingText(data.Match.Blue1, data.Rankings));
-  $("#" + blueSide + "FinalTeam2").html(data.Match.Blue2 + "" + getRankingText(data.Match.Blue2, data.Rankings));
-  $("#" + blueSide + "FinalTeam3").html(data.Match.Blue3 + "" + getRankingText(data.Match.Blue3, data.Rankings));
+  //$("#" + blueSide + "FinalTeam1").html(data.Match.Blue1 + "" + getRankingText(data.Match.Blue1, data.Rankings));
+  //$("#" + blueSide + "FinalTeam2").html(data.Match.Blue2 + "" + getRankingText(data.Match.Blue2, data.Rankings));
+  //$("#" + blueSide + "FinalTeam3").html(data.Match.Blue3 + "" + getRankingText(data.Match.Blue3, data.Rankings));
+  $("#" + blueSide + "FinalTeam1").html(getRankingText(data.Match.Blue1, data.Rankings) + "" + data.Match.Blue1);
+  $("#" + blueSide + "FinalTeam2").html(getRankingText(data.Match.Blue2, data.Rankings) + "" + data.Match.Blue2);
+  $("#" + blueSide + "FinalTeam3").html(getRankingText(data.Match.Blue3, data.Rankings) + "" + data.Match.Blue3);
   $("#" + blueSide + "FinalTeam1Avatar").attr("src", getAvatarUrl(data.Match.Blue1));
   $("#" + blueSide + "FinalTeam2Avatar").attr("src", getAvatarUrl(data.Match.Blue2));
   $("#" + blueSide + "FinalTeam3Avatar").attr("src", getAvatarUrl(data.Match.Blue3));
@@ -479,6 +485,20 @@ var setPowerCellText = function(element, scoreSummary, stage) {
   }
   element.html(text);
   element.css("opacity", opacity);
+};
+
+var getNewRankingText = function(teamId, rankings) {
+  var ranking = rankings[teamId];
+  if (ranking === undefined || ranking.Rank === 0) {
+    return "";
+  }
+
+  if (ranking.Rank > ranking.PreviousRank && ranking.PreviousRank > 0) {
+    return "<div class='rank-box rank-up'>" + ranking.Rank + "</div><div class='arrow-up'></div>";
+  } else if (ranking.Rank < ranking.PreviousRank) {
+    return "<div class='rank-box rank-down'>" + ranking.Rank + "</div><div class='arrow-down'></div>";
+  }
+  return "<div class='rank-box rank-same'>" + ranking.Rank + "</div>";
 };
 
 // Returns the string to be displayed next to the team number on the final score screen, to indicate change in rank.
