db7aab8aa434da77457ca70bf1389ba3efab687e
Fletcher Salesky <fletcher@salesky.dev>
2023-11-29 12:21:51 -0800

    Update DHCP Ranges (#179)
    * Update DHCP Ranges
    
    Updated DHCP ranges to match FIRST FMS config
    https://docs.wpilib.org/en/stable/docs/networking/networking-introduction/networking-basics.html#ethernet-wireless
    
    * Update switch.go to fix failed DHCP config test
    
    * run go fmt ./...

--- network/switch_test.go
+++ network/switch_test.go
@@ -42,7 +42,7 @@ func TestConfigureSwitch(t *testing.T) {
 	assert.Equal(
 		t,
 		"password\nenable\npassword\nterminal length 0\nconfig terminal\n"+
-			"ip dhcp excluded-address 10.2.54.1 10.2.54.100\nip dhcp pool dhcp50\n"+
+			"ip dhcp excluded-address 10.2.54.1 10.2.54.19\nip dhcp excluded-address 10.2.54.200 10.2.54.254\nip dhcp pool dhcp50\n"+
 			"network 10.2.54.0 255.255.255.0\ndefault-router 10.2.54.4\nlease 7\n"+
 			"access-list 150 permit ip 10.2.54.0 0.0.0.255 host 10.0.100.5\n"+
 			"access-list 150 permit udp any eq bootpc any eq bootps\n"+
@@ -62,32 +62,32 @@ func TestConfigureSwitch(t *testing.T) {
 	assert.Equal(
 		t,
 		"password\nenable\npassword\nterminal length 0\nconfig terminal\n"+
-			"ip dhcp excluded-address 10.11.14.1 10.11.14.100\nip dhcp pool dhcp10\n"+
+			"ip dhcp excluded-address 10.11.14.1 10.11.14.19\nip dhcp excluded-address 10.11.14.200 10.11.14.254\nip dhcp pool dhcp10\n"+
 			"network 10.11.14.0 255.255.255.0\ndefault-router 10.11.14.4\nlease 7\n"+
 			"access-list 110 permit ip 10.11.14.0 0.0.0.255 host 10.0.100.5\n"+
 			"access-list 110 permit udp any eq bootpc any eq bootps\n"+
 			"interface Vlan10\nip address 10.11.14.4 255.255.255.0\n"+
-			"ip dhcp excluded-address 10.2.54.1 10.2.54.100\nip dhcp pool dhcp20\n"+
+			"ip dhcp excluded-address 10.2.54.1 10.2.54.19\nip dhcp excluded-address 10.2.54.200 10.2.54.254\nip dhcp pool dhcp20\n"+
 			"network 10.2.54.0 255.255.255.0\ndefault-router 10.2.54.4\nlease 7\n"+
 			"access-list 120 permit ip 10.2.54.0 0.0.0.255 host 10.0.100.5\n"+
 			"access-list 120 permit udp any eq bootpc any eq bootps\n"+
 			"interface Vlan20\nip address 10.2.54.4 255.255.255.0\n"+
-			"ip dhcp excluded-address 10.2.96.1 10.2.96.100\nip dhcp pool dhcp30\n"+
+			"ip dhcp excluded-address 10.2.96.1 10.2.96.19\nip dhcp excluded-address 10.2.96.200 10.2.96.254\nip dhcp pool dhcp30\n"+
 			"network 10.2.96.0 255.255.255.0\ndefault-router 10.2.96.4\nlease 7\n"+
 			"access-list 130 permit ip 10.2.96.0 0.0.0.255 host 10.0.100.5\n"+
 			"access-list 130 permit udp any eq bootpc any eq bootps\n"+
 			"interface Vlan30\nip address 10.2.96.4 255.255.255.0\n"+
-			"ip dhcp excluded-address 10.15.3.1 10.15.3.100\nip dhcp pool dhcp40\n"+
+			"ip dhcp excluded-address 10.15.3.1 10.15.3.19\nip dhcp excluded-address 10.15.3.200 10.15.3.254\nip dhcp pool dhcp40\n"+
 			"network 10.15.3.0 255.255.255.0\ndefault-router 10.15.3.4\nlease 7\n"+
 			"access-list 140 permit ip 10.15.3.0 0.0.0.255 host 10.0.100.5\n"+
 			"access-list 140 permit udp any eq bootpc any eq bootps\n"+
 			"interface Vlan40\nip address 10.15.3.4 255.255.255.0\n"+
-			"ip dhcp excluded-address 10.16.78.1 10.16.78.100\nip dhcp pool dhcp50\n"+
+			"ip dhcp excluded-address 10.16.78.1 10.16.78.19\nip dhcp excluded-address 10.16.78.200 10.16.78.254\nip dhcp pool dhcp50\n"+
 			"network 10.16.78.0 255.255.255.0\ndefault-router 10.16.78.4\nlease 7\n"+
 			"access-list 150 permit ip 10.16.78.0 0.0.0.255 host 10.0.100.5\n"+
 			"access-list 150 permit udp any eq bootpc any eq bootps\n"+
 			"interface Vlan50\nip address 10.16.78.4 255.255.255.0\n"+
-			"ip dhcp excluded-address 10.15.38.1 10.15.38.100\nip dhcp pool dhcp60\n"+
+			"ip dhcp excluded-address 10.15.38.1 10.15.38.19\nip dhcp excluded-address 10.15.38.200 10.15.38.254\nip dhcp pool dhcp60\n"+
 			"network 10.15.38.0 255.255.255.0\ndefault-router 10.15.38.4\nlease 7\n"+
 			"access-list 160 permit ip 10.15.38.0 0.0.0.255 host 10.0.100.5\n"+
 			"access-list 160 permit udp any eq bootpc any eq bootps\n"+
