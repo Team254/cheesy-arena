5f1eeba6b60512b9dc4537ca9a301014dd4f1cd0
Ian Thompson <ian@yann.io>
2025-05-18 09:07:37 -0700

    Add score summary to the HR display

--- static/js/referee_panel.js
+++ static/js/referee_panel.js
@@ -72,12 +72,26 @@ var handleMatchLoad = function(data) {
   setTeamCard("blue", 1, data.Teams["B1"]);
   setTeamCard("blue", 2, data.Teams["B2"]);
   setTeamCard("blue", 3, data.Teams["B3"]);
+
+  $("#redScoreSummary .team-1").text(data.Teams["R1"].Id);
+  $("#redScoreSummary .team-2").text(data.Teams["R2"].Id);
+  $("#redScoreSummary .team-3").text(data.Teams["R3"].Id);
+  $("#blueScoreSummary .team-1").text(data.Teams["B1"].Id);
+  $("#blueScoreSummary .team-2").text(data.Teams["B2"].Id);
+  $("#blueScoreSummary .team-3").text(data.Teams["B3"].Id);
 };
 
 // Handles a websocket message to update the match status.
 const handleMatchTime = function(data) {
   $(".control-button").attr("data-enabled", matchStates[data.MatchState] === "POST_MATCH");
 };
+
+const endgameStatusNames = [
+  "None",
+  "Park",
+  "Shallow",
+  "Deep",
+];
 
 // Handles a websocket message to update the realtime scoring fields.
 const handleRealtimeScore = function(data) {
@@ -93,6 +107,34 @@ const handleRealtimeScore = function(data) {
     fetch("/panels/referee/foul_list")
       .then(response => response.text())
       .then(svg => $("#foulList").html(svg));
+  }
+
+  for (alliance of ["red", "blue"]) {
+    let score;
+    if (alliance === "red") {
+      score = data.Red.Score;
+    } else {
+      score = data.Blue.Score;
+    }
+
+    let l1_total = score.Reef.TroughNear + score.Reef.TroughFar;
+    let l2_total = score.Reef.Branches[0].filter(Boolean).length;
+    let l3_total = score.Reef.Branches[1].filter(Boolean).length;
+    let l4_total = score.Reef.Branches[2].filter(Boolean).length;
+
+    let scoreRoot = `${alliance}ScoreSummary`;
+    $(`#${scoreRoot} .team-1-leave`).text(score.LeaveStatuses[0] ? "✓" : "❌");
+    $(`#${scoreRoot} .team-2-leave`).text(score.LeaveStatuses[1] ? "✓" : "❌");
+    $(`#${scoreRoot} .team-3-leave`).text(score.LeaveStatuses[2] ? "✓" : "❌");
+    $(`#${scoreRoot} .team-1-endgame`).text(endgameStatusNames[score.EndgameStatuses[0]]);
+    $(`#${scoreRoot} .team-2-endgame`).text(endgameStatusNames[score.EndgameStatuses[1]]);
+    $(`#${scoreRoot} .team-3-endgame`).text(endgameStatusNames[score.EndgameStatuses[2]]);
+    $(`#${scoreRoot} .coral-l1`).text(l1_total);
+    $(`#${scoreRoot} .coral-l2`).text(l2_total);
+    $(`#${scoreRoot} .coral-l3`).text(l3_total);
+    $(`#${scoreRoot} .coral-l4`).text(l4_total);
+    $(`#${scoreRoot} .processor`).text(score.ProcessorAlgae);
+    $(`#${scoreRoot} .barge`).text(score.BargeAlgae);
   }
 }
 
