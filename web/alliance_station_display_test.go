d1328dd92e5839dbd7306feb0444f975a5584fd2
cpapplefamily <cpapplefamily@gmail.com>
2025-05-21 22:18:10 -0500

    Add Help Files to Freezy Arena. FTA color definitions and Alliance station selection PDF

--- web/alliance_station_display_test.go
+++ web/alliance_station_display_test.go
@@ -60,16 +60,18 @@ func TestAllianceStationDisplayWebsocket(t *testing.T) {
 	web.arena.AllianceStations["B3"].Bypass = true
 	web.arena.StartMatch()
 	web.arena.Update()
-	messages := readWebsocketTypes(t, ws, 8, "matchTime")
+	messages := readWebsocketTypes(t, ws, 5, "matchTime")
 	_, ok := messages["matchTime"]
 	assert.True(t, ok)
 	web.arena.MatchStartTime = time.Now().Add(-time.Duration(game.MatchTiming.AutoDurationSec) * time.Second)
 	web.arena.Update()
-	messages = readWebsocketTypes(t, ws, 8, "arenaStatus", "matchTime")
+	messages = readWebsocketTypes(t, ws, 5, "arenaStatus", "matchTime")
 	_, ok = messages["arenaStatus"]
 	assert.True(t, ok)
 	_, ok = messages["matchTime"]
 	assert.True(t, ok)
 	web.arena.RealtimeScoreNotifier.Notify()
-	readWebsocketTypeEventually(t, ws, "realtimeScore", 2)
+	messages = readWebsocketTypes(t, ws, 5, "realtimeScore")
+	_, ok = messages["realtimeScore"]
+	assert.True(t, ok)
 }
