6f35fb70a2b8045ed170b3cf639d0eb621bc1660
LiKai_Huang <moray.huang@gmail.com>
2026-02-12 15:37:50 +0800

    Revert "referee panel update"
    This reverts commit 329645d643b34e25b89a4e983b3f5b411ccc1f81.

--- cleanup-ansys-DESKTOP-1H0JOJ6-8168.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-set LOCALHOST=%COMPUTERNAME%
-if /i "%LOCALHOST%"=="DESKTOP-1H0JOJ6" (taskkill /f /pid 9284)
-if /i "%LOCALHOST%"=="DESKTOP-1H0JOJ6" (taskkill /f /pid 21412)
-if /i "%LOCALHOST%"=="DESKTOP-1H0JOJ6" (taskkill /f /pid 8688)
-if /i "%LOCALHOST%"=="DESKTOP-1H0JOJ6" (taskkill /f /pid 8168)
-
-del /F cleanup-ansys-DESKTOP-1H0JOJ6-8168.bat
